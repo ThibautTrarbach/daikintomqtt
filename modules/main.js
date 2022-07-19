9cd7aaf94d3cefc3264a4431a78f9a94b687ce45
Thibaut Trarbach <30575266+ThibautTrarbach@users.noreply.github.com>
2022-07-19 22:08:58 +0200

    Update Action

--- modules/main.js
+++ modules/main.js
@@ -18,7 +18,10 @@ async function getDataFromModules(devices, dataDirectory) {
 }
 
 async function setDataFromModules(devices, message) {
-    switch (devices.getData('gateway', 'modelInfo').value) {
+    let value;
+    if (devices.getData('gateway', 'modelInfo') !== null) value = devices.getData('gateway', 'modelInfo').value
+    else if (devices.getData('0', 'modelInfo') !== null) value = devices.getData('0', 'modelInfo').value
+    switch (value) {
         case 'BRP069C4x':
             return await setBRP069C4x(devices, message);
         case 'BRP069A62':
