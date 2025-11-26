b16b4f31c834526794086904b52189f0c959c2fd
Thibaut Trarbach <30575266+ThibautTrarbach@users.noreply.github.com>
2025-11-26 23:09:20 +0100

    Update firmware version in SystemBridge to 1.3.0

--- src/modules/gateway/SystemBridge.ts
+++ src/modules/gateway/SystemBridge.ts
@@ -10,7 +10,7 @@ export class SystemBridge implements ClassModule {
 		name: "Daikin2MQTT Bridge",
 		modelInfo: "Daikin2MQTT",
 		serialNumber: "Daikin2MQTT",
-		firmwareVersion: "1.0.0",
+		firmwareVersion: "1.3.0",
 		isInErrorState: "false",
 		errorCode: ""
 	}
