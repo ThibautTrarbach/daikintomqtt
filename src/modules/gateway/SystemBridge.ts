5b9df969c8210c5281f8744493c260cd34458aa3
Thibaut Trarbach <30575266+ThibautTrarbach@users.noreply.github.com>
2025-12-04 22:21:58 +0100

    Update version numbers for Daikin2MQTT to 2.0.0
    - Bumped the version in package.json from 1.3.0 to 2.0.0 to reflect the new release.
    - Bumped the version in SystemBridge.ts to 2.0.0 for consistency with the package version.

--- src/modules/gateway/SystemBridge.ts
+++ src/modules/gateway/SystemBridge.ts
@@ -9,7 +9,7 @@ export class SystemBridge implements ClassModule {
 		name: "Daikin2MQTT Bridge",
 		modelInfo: "Daikin2MQTT",
 		serialNumber: "Daikin2MQTT",
-		firmwareVersion: "1.3.0",
+		firmwareVersion: "2.0.0",
 		isInErrorState: "false",
 		errorCode: ""
 	}
