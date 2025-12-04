42db6959335241b7cc9461c4b42b83cc3605cdef
ThibautTrarbach <30575266+ThibautTrarbach@users.noreply.github.com>
2025-12-04 21:27:31 +0000

    Update release-alpha to output generated at 74892f6

--- modules/gateway/SystemBridge.js
+++ modules/gateway/SystemBridge.js
@@ -19,7 +19,7 @@ class SystemBridge {
         name: "Daikin2MQTT Bridge",
         modelInfo: "Daikin2MQTT",
         serialNumber: "Daikin2MQTT",
-        firmwareVersion: "1.3.0",
+        firmwareVersion: "2.0.0",
         isInErrorState: "false",
         errorCode: ""
     };
