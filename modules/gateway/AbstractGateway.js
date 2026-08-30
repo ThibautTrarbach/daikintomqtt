1e3348b6efc08304c0d2efe3a4fccfaaeee6cb92
ThibautTrarbach <30575266+ThibautTrarbach@users.noreply.github.com>
2026-08-30 18:44:01 +0000

    Update release-alpha to output generated at be2138e

--- modules/gateway/AbstractGateway.js
+++ modules/gateway/AbstractGateway.js
@@ -9,6 +9,7 @@ class AbstractGateway {
         (0, metadataRegistry_1.registerCharacteristics)(this, characteristics);
         (0, metadataRegistry_1.registerDeviceMetadata)(this, deviceKey, deviceMetadata);
         (0, metadataRegistry_1.installGatewayProperties)(this, characteristics);
+        this._device = {};
         (0, BaseModules_1.convertDaikinDevice)(device, this);
     }
     set device(value) {
